8d6e004aafcb81c6b7a9e2744c402863197b034a
TIROK547 <ilia.gotbi.2006@gmail.com>
2025-02-08 21:18:36 +0330

    Check updated  -0.1

--- check.py
+++ check.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 game = [
       [0, 2, 0, 6, 0, 8, 0, 0, 0],
       [5, 8, 0, 0, 0, 9, 7, 0, 0],
@@ -7,8 +8,15 @@ game = [
       [0, 0, 8, 0, 0, 0, 0, 1, 3],
       [0, 0, 0, 0, 2, 0, 0, 0, 0],
       [0, 0, 9, 8, 0, 0, 0, 3, 6],
-      [0, 0, 0, 3, 0, 6, 0, 9, 0]
+      [1, 0, 0, 3, 0, 6, 0, 9, 0]
     ]
 def check_game(arr,user_choice):
-    print()
-print(check_game(game,[1,9]))
+        my_game = arr
+        row = user_choice[1]
+        col = user_choice[0]
+        if arr[row][col] == 0:
+            my_game[row][col] = user_choice[2]
+            return my_game
+        elif arr[row][col] != 0:
+            return [False,"This is not a valid choice"]
+pprint(check_game(game,[1-1,9-1,1]))
