7f004c58c6089227bec010d5772346cc49448d34
TIROK547 <ilia.gotbi.2006@gmail.com>
2025-02-08 22:15:27 +0330

    main got optimized for check update

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print([],[1,3,6],None)
+from check import check_game
+#check input data: check_game(game_arr, [row, col, num])
+#check output data: return [new_game_arr, game_winner, game_message, user_choices]
+difficulty = int(input("Choose difficulty: easy(1), medium(2), hard(3): "))
+user_choices = list(map(int,input("Enter your choice: ").split()))
