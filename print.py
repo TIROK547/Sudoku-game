7df84e0660e605581c179b18125db18dfb09a6b5
amir <amir7617.ahm@gmail.com>
2025-02-10 02:14:23 +0330

    fixed and colors added

--- print.py
+++ print.py
@@ -10,6 +10,7 @@ mode_1= [
       [0, 6, 0, 0, 0, 0, 2, 8, 0],
       [0, 0, 0, 4, 1, 9, 0, 0, 5],
       [0, 0, 0, 0, 8, 0, 0, 7, 9] ]
+
 #color_codes
 #YELLOW = '\033[33m'
 #RED = '\033[31m'
@@ -18,26 +19,28 @@ mode_1= [
 
 
 user_choice = [0, 0, None]
-#[],[1,3,6],None
-#function:
+
 def print_game(game_board,game_case,message,user_choice):
+      row , col, num = user_choice
+      game_board[row][col] = num
+
+
       for i in range(9) :
            if i % 3 == 0 and i != 0:
-                  print("_" * 21) 
+              print("_" * 21) 
+
            for j in range(9):
                if j % 3 == 0 and j != 0 :
                   print("|", end = "")
-               if game_board[i][j] != 0:
-                        print(game_board[i][j], end =" ")
-                
-               else:
-                        print(".", end = " ")     
-           print() 
+      
+               if (i, j) == (row, col):
+                  if game_case is None:   
+                        print('\033[0m'+ '\033[33m'+ str(num) +'\033[0m', end="")
+                  elif game_case is False:  
+                        print('\033[0m' + '\033[31m' + str(num) + '\033[0m', end="")
+               else:  
+                    print(game_board[i][j] if game_board[i][j] != 0 else ".", end=" ")    
+           print()
 
-           if game_case == True:
-                 user_choice[i][j] == num
-                 print('\033[0m', '\033[33m', num, '\033[0m')
-           elif game_case == False:
-                user_choice[i][j] == num
-                print('\033[0m', '\033[31m', num, '\033[0m')                  
-print_game(mode_1,None,"congrats",[0,2,1])
+
+print_game(mode_1, None, "congarts", [0,8,8])
