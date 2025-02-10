9364909f28b51361ed85faaff8d782082ed2bb3f
TIROK547 <ilia.gotbi.2006@gmail.com>
2025-02-10 12:17:53 +0330

    check and print finished

--- check.py
+++ check.py
@@ -1,3 +1,17 @@
+from print import print_game
+
+
+easy_sudoku_boards = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
+                      [6, 0, 0, 1, 9, 5, 0, 0, 0],
+                      [0, 9, 8, 0, 0, 0, 0, 6, 0],
+                      [8, 0, 0, 0, 6, 0, 0, 0, 3],
+                      [4, 0, 0, 8, 0, 3, 0, 0, 1],
+                      [7, 0, 0, 0, 2, 0, 0, 0, 6],
+                      [0, 6, 0, 0, 0, 0, 2, 8, 0],
+                      [0, 0, 0, 4, 1, 9, 0, 0, 5],
+                      [0, 0, 0, 0, 8, 0, 0, 7, 9]]
+
+
 def check_game(arr, user_choice):
     row, col, num = user_choice
     message = ""
@@ -23,4 +37,6 @@ def check_game(arr, user_choice):
                 message = "Congratulations! You won!"
                 game_case = True
                 
-    return [arr, game_case, message, user_choice]
+    return (arr, game_case, message, user_choice)
+x = check_game(easy_sudoku_boards,[0,2,1])
+print_game(x[0],x[1],x[2],x[3])
--- print.py
+++ print.py
@@ -1,16 +1,5 @@
 from pprint import pprint
 
-mode_1= [ 
-      [5, 3, 0, 0, 7, 0, 0, 0, 0],
-      [6, 0, 0, 1, 9, 5, 0, 0, 0],
-      [0, 9, 8, 0, 0, 0, 0, 6, 0],
-      [8, 0, 0, 0, 6, 0, 0, 0, 3],
-      [4, 0, 0, 8, 0, 3, 0, 0, 1],
-      [7, 0, 0, 0, 2, 0, 0, 0, 6],
-      [0, 6, 0, 0, 0, 0, 2, 8, 0],
-      [0, 0, 0, 4, 1, 9, 0, 0, 5],
-      [0, 0, 0, 0, 8, 0, 0, 7, 9] ]
-
 #color_codes
 #YELLOW = '\033[33m'
 #RED = '\033[31m'
@@ -35,12 +24,10 @@ def print_game(game_board,game_case,message,user_choice):
       
                if (i, j) == (row, col):
                   if game_case is None:   
-                        print('\033[0m'+ '\033[33m'+ str(num) +'\033[0m', end="")
+                        print('\033[0m'+ '\033[33m'+ str(num) +'\033[0m' + " ", end="")
                   elif game_case is False:  
-                        print('\033[0m' + '\033[31m' + str(num) + '\033[0m', end="")
+                        print('\033[0m' + '\033[31m' + str(num) + '\033[0m'+" ", end="")
                else:  
                     print(game_board[i][j] if game_board[i][j] != 0 else ".", end=" ")    
            print()
-
-
-print_game(mode_1, None, "congarts", [0,8,8])
+           
